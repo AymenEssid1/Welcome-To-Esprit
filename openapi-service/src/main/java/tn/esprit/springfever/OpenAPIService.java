b5228ed0b625a430f26a62c44e3f66b66870b8e9
Ahmed-Debbiche007 <ahmed.debbiche@esprit.tn>
2023-02-18 14:13:41 +0100

    prep

--- openapi-service/src/main/java/tn/esprit/springfever/OpenAPIService.java
+++ openapi-service/src/main/java/tn/esprit/springfever/OpenAPIService.java
@@ -1,6 +1,5 @@
 package tn.esprit.springfever;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -11,8 +10,8 @@ import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 @EnableSpringConfigured
 @EntityScan (basePackages = {"tn.esprit.springfever.entities"})
 @EnableEurekaClient
-public class UserService {
+public class OpenAPIService {
     public static void main(String[] args) {
-        SpringApplication.run(UserService.class, args);
+        SpringApplication.run(OpenAPIService.class, args);
     }
 }
