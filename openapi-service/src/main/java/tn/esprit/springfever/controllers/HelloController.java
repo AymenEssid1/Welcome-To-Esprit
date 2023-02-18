10bec9519955cf1bc57b5e150db1707060b941b1
ahmedg99 <gouiaaahma@gmail.com>
2023-02-18 18:16:19 +0100

    add evaluation branch

---
+++ openapi-service/src/main/java/tn/esprit/springfever/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package tn.esprit.springfever.controllers;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "Hello World")
+@Service
+@RequestMapping("/api/user")
+public class HelloController {
+    @GetMapping
+    public String hello(){
+        return "Hello from User Service";
+    }
+}
