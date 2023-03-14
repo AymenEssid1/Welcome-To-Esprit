c70b44e428bdf2c11b8a96424db7863296a52926
Ahmed-Debbiche007 <ahmed.debbiche@esprit.tn>
2023-03-14 05:49:58 +0100

    add: ci pipelines

--- script.sh
+++ script.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-
+PROJECT="welcome-to-esprit"
+if [ -d $PROJECT ]; then
+    echo $PROJECT exists.
+    cd $PROJECT
+    git fetch origin main
+    git pull origin main
+else
+    git clone git@gitlab.com:ahmedg99/welcome-to-esprit.git
+    cd $PROJECT
+fi
 #mvn clean package -Dmaven.test.skip
 docker-compose down -v --remove-orphans
 docker-compose up --build -d
